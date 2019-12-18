b4c67cf3c0871bd6522f357d9034dd8ad05e3982
AllergicGorilla <viniciusC14H18N2O5@gmail.com>
2019-12-18 15:03:25 -0300

    Remove unecessary files

--- newton_physics.gd
+++
@@ -1,7 +0,0 @@
-class_name Newton
-const G = 500000.0
-#Represents force applied on M1
-#therefore the distance vector must point from M2 to M1
-static func gravitational_force(M1:float , M2: float, distanceVec: Vector2) -> Vector2:
-	var distance : float = distanceVec.length()
-	return -(G*M1*M2/(distance*distance*distance))*distanceVec
