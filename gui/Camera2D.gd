2b088527978033f6870f893a2ba900f28332247d
AllergicGorilla <viniciusC14H18N2O5@gmail.com>
2019-12-18 18:10:26 -0300

    Added camera zoom

--- gui/Camera2D.gd
+++ gui/Camera2D.gd
@@ -4,8 +4,11 @@ onready var tools = get_node("/root/Tools")
 onready var Body = tools.currentPlanetBody
 
 const speed = 200.0
-
+const unit = Vector2(1.0,1.0)
+var zoomFactor = 1.0
+const zoomDelta = 0.05
 func _ready():
+	zoom = zoomFactor*unit
 	tools.connect("currentPlanetBody_changed",self,"_on_currentPlanetBody_changed")
 
 func _process(delta):
@@ -20,5 +23,15 @@ func _process(delta):
 			position.x += speed*delta
 		if Input.is_action_pressed("camera_left"):
 			position.x -= speed*delta
+func _input(event):
+	if event.is_action_pressed("camera_reset_zoom"):
+		zoomFactor = 1.0
+	if event.is_action_pressed("mouse_scroll_up"):
+		zoomFactor -= zoomDelta
+	elif event.is_action_pressed("mouse_scroll_down"):
+		zoomFactor += zoomDelta
+	zoomFactor = clamp(zoomFactor, 0.01, 10.0)
+	zoom = zoomFactor * unit
+
 func _on_currentPlanetBody_changed():
 	Body = tools.currentPlanetBody
